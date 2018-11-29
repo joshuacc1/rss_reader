ddb3237905a296ce513ececa13a77157ca929a8c
combsjc1 <joshua.combs@jhuapl.edu>
2018-11-29 10:21:38 -0500

    Added Features to processing

--- RSS_Manager/Processing.py
+++ RSS_Manager/Processing.py
@@ -1,12 +1,15 @@
-from nltk import word_tokenize, Text
+from nltk import word_tokenize, Text, pos_tag
 import feedparser
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 from requests import request
 from nltk import word_tokenize, Counter, pos_tag, sent_tokenize
 from nltk.text import ContextIndex
+from nltk.chunk.regexp import ChunkRule, ExpandLeftRule, ExpandRightRule, UnChunkRule
+from nltk.chunk import RegexpChunkParser
 from sklearn.feature_extraction.text import CountVectorizer
-from database import DatabaseManagement
+import json
+#from database import DatabaseManagement
 
 import pprint
 
@@ -19,6 +22,14 @@ class RSSNLTK:
         self.feedparser = None
         self.tokentext = None
         self.senttokenizedtext = None
+
+    def savetojson(self, filename):
+        with open(filename, 'w') as f:
+            json.dump(self.texts)
+
+    def loadjson(self, filename):
+        with open(filename) as f:
+            self.texts = json.load(f)
 
     def initializeRSS(self, rsslink):
         self.rsslink = rsslink
@@ -46,8 +57,6 @@ class RSSNLTK:
             cleantexts = [text for text in visibletext if len(text.split(' ')) > sensitivity]
             mergedtext = ' '.join(cleantexts)
             self.texts['htmltexts'].append(mergedtext)
-
-
 
     def tokenizetext(self):
         tokentexts = {}
@@ -128,6 +137,56 @@ class RSSNLTK:
             if 'NNP' == words[word]['tag']:
                 print(words[word]['count'], words[word]['word'])
 
+    def countwords(self, text):
+        words = {}
+        maxcount = 0
+        postag =  pos_tag(word_tokenize(text))
+        for i in postag:
+            if not i[0] in words:
+                words[i[0]] = {'count': 1, 'word': i[0], 'tag': i[1]}
+            else:
+                words[i[0]]['count'] += 1
+                if words[i[0]]['count'] > maxcount:
+                    maxcount = words[i[0]]['count']
+
+        for word in words:
+            words[word]['count'] = words[word]['count']/maxcount
+
+        words = sorted(words.values(), key=lambda x: x['count'])
+        words = {x['word']: x for x in words}
+
+        return words
+
+    def scoresentences(self,text):
+        words = self.countwords(text)
+
+        senttk = sent_tokenize(text)
+        scoredsentences = []
+        for sent in senttk:
+            sentscore = 0
+            for word in word_tokenize(sent):
+                sentscore += words[word]['count']
+            scoredsentences.append({'sentence': sent, 'score': sentscore})
+
+        #csenttks = [{'sentence': x, 'count': 1} for x in senttks]
+
+        # for senttk in senttks:
+        #     tscore = 0
+        #     for word in word_tokenize(senttk):
+        #         tscore += words[word]
+
+        return scoredsentences
+
+    def summarizetexts(self,texttype):
+        textsummaries = []
+        for text in self.texts[texttype]:
+            counts = self.scoresentences(text)
+            scounts = sorted(counts, key=self.filtercount)
+            print(scounts[-1])
+
+    def filtercount(self,word):
+        return word['score']
+
     def tag_visible(self, element):
         if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
             return False
@@ -135,24 +194,53 @@ class RSSNLTK:
             return False
         return True
 
-
-
-
 if __name__ == "__main__":
     rssproc = RSSNLTK()
-    rssproc.initializeRSS("http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml")
-    rssproc.initRSSdatabase()
-    rssproc.getHTMLlinktext()
+    rssproc.loadjson('junk.json')
     rssproc.tokenizetext()
     rssproc.senttokenizetext()
-    print(rssproc.senttokenizedtext['htmltexts'][10])
-    print(rssproc.makevector(rssproc.senttokenizedtext['htmltexts'][10]))
-    #rssproc.getallnouns()
-    #rssproc.getsimiliar("angry")
+    rssproc.summarizetexts('htmltexts')
+
+    # filcounts = filter(rssproc.filtercount,counts.values())
+    # highestcount = [x for x in filcounts][-1]['count']
+    # filcounts = filter(rssproc.filtercount, counts.values())
+    # filweights = {}
+    # for x in filcounts:
+    #     item =x
+    #     item['count'] = item['count']/highestcount
+    #     filweights[x['word']] = item
+    # print([x for x in filweights.values()])
+    senttokens = rssproc.senttokenizedtext['htmltexts'][10]
+
+    s = 'there are 12 boxes in the closet'
+
+    ur = ChunkRule('<CD>', 'single noun')
+    el = ExpandLeftRule('<NNS>', '<CD>', 'get left determiner')
+    er = ExpandRightRule('<CD>', '<NNS>', 'get right plural noun')
+    un = UnChunkRule('<DT><NN.*>*', 'unchunk everything')
+
+    chunker = RegexpChunkParser([ur,el,er])
+
+    print(chunker.parse(pos_tag(word_tokenize(s))))
+
+    d = []
+    for sent in senttokens:
+        tk = word_tokenize(sent)
+        tkpos = pos_tag(tk)
+        for x in tkpos:
+
+            if 'CD' in x:
+                w = chunker.parse(tkpos)
+                d.append(w)
+                break
+    for x in d:
+        #print(x)
+        pass
+
+
     for match in rssproc.textconcordance("election", "summaries"):
         if match:
          print(match)
-    for match in rssproc.textsimiliar("president","htmltexts"):
+    for match in rssproc.textsimiliar("Peloci","htmltexts"):
         if match:
             print(match)
-    #rssproc.returntokentext()
